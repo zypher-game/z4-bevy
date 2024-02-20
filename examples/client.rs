7ab7a20831cb5dd0f088b9adcffd7a5799286d53
Neo Sun <huachuang20@gmail.com>
2024-02-20 19:23:02 +1300

    Add z4 plugin for bevy

---
+++ examples/client.rs
@@ -0,0 +1,39 @@
+use bevy::prelude::*;
+use z4_bevy::{ws_connect, PeerKey, RecvError, WsConnection, Z4ClientPlugin};
+
+const MY_ROOM_ID: u64 = 1;
+const MY_SERVER: &str = "ws://127.0.0.1:8000";
+
+#[derive(Component)]
+struct Name(String);
+
+fn main() {
+    App::new()
+        //.add_plugins(DefaultPlugins)
+        .add_plugins(Z4ClientPlugin)
+        .add_systems(Startup, init_ws_connect)
+        .add_systems(Update, ws_receive)
+        .run();
+}
+
+fn init_ws_connect(mut commands: Commands) {
+    let peer_key = PeerKey::default();
+    println!("=====");
+    ws_connect(&mut commands, MY_SERVER, &peer_key, MY_ROOM_ID);
+}
+
+fn ws_receive(mut commands: Commands, connections: Query<(Entity, &WsConnection)>) {
+    for (entity, conn) in connections.iter() {
+        match conn.recv() {
+            Ok(message) => {
+                println!("message: {}", message);
+                // conn.send(message);
+            }
+            Err(RecvError::Empty) => break,
+            Err(RecvError::Closed) => {
+                commands.entity(entity).despawn();
+                break;
+            }
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
-use tdn_bevy::{RecvError, WsClient, WsClientPlugin, WsConnection};
+use serde_json::{from_str, to_string, Value};
+use tdn::types::rpc::rpc_request;
+use tdn_bevy::{Message, WsClientPlugin};
+
+pub type RoomId = u64;
+
+pub use tdn::prelude::PeerKey;
+pub use tdn_bevy::{RecvError, WsClient, WsConnection};
 
 pub struct Z4ClientPlugin;
 
@@ -9,24 +16,35 @@ impl Plugin for Z4ClientPlugin {
     }
 }
 
-fn connect_ws(mut commands: Commands, ws_client: Res<WsClient>) {
-    ws_client.connect(&mut commands, "127.0.0.1:8000", None);
+#[inline]
+pub fn ws_connect(commands: &mut Commands, url: &str, peer: &PeerKey, room: RoomId) {
+    // build the z4 connect init message
+    let msg = build_request("connect", vec![], peer, room);
+    WsClient.connect(commands, url, Some(msg));
 }
 
-fn receive_message(mut commands: Commands, connections: Query<(Entity, &WsConnection)>) {
-    for (entity, conn) in connections.iter() {
-        loop {
-            match conn.recv() {
-                Ok(message) => {
-                    println!("message: {}", message);
-                    conn.send(message);
-                }
-                Err(RecvError::Empty) => break,
-                Err(RecvError::Closed) => {
-                    commands.entity(entity).despawn();
-                    break;
-                }
-            }
+#[inline]
+pub fn build_request(method: &str, v: Vec<Value>, peer: &PeerKey, room: RoomId) -> Message {
+    let mut request = rpc_request(0, &method, v, room);
+    request
+        .as_object_mut()
+        .unwrap()
+        .insert("peer".to_owned(), peer.peer_id().to_hex().into());
+    Message::from(to_string(&request).unwrap_or("".to_owned()))
+}
+
+#[inline]
+pub fn parse_response(msg: &Message) -> Result<(RoomId, String, Vec<Value>), String> {
+    let msg = msg.to_text().unwrap_or("");
+    match from_str::<Value>(&msg) {
+        Ok(mut values) => {
+            let gid = values["gid"].as_u64().unwrap(); // TODO unwrap
+            let method = values["method"].as_str().unwrap().to_owned();
+            // let server_id = values["peer"].as_str().unwrap(); TODO
+            let tmp = values["result"].take().as_array().unwrap().to_vec();
+            return Ok((gid, method, tmp));
         }
+        Err(_e) => {}
     }
+    Err(String::from("Invalid"))
 }
