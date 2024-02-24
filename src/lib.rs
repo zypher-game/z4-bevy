56f89f3d052dd1bf280ebce6228e688ed80ba07b
Neo Sun <huachuang20@gmail.com>
2024-02-24 23:48:19 +1300

    Support RoomMarket

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 use bevy::prelude::*;
-use serde_json::{from_str, to_string, Value};
+use serde::Deserialize;
+use serde_json::{from_str, json, to_string, Value};
 
 pub type RoomId = u64;
 pub use tdn_bevy::RecvError;
 pub use tdn_types::{primitives::PeerKey, rpc::rpc_request};
+
+#[cfg(feature = "wasm")]
+use tdn_bevy::wasm::{HttpClient, HttpConnection, WasmClientPlugin};
 
 pub struct Z4ClientPlugin;
 
@@ -13,7 +17,69 @@ impl Plugin for Z4ClientPlugin {
         app.add_plugins(tdn_bevy::ws::WsClientPlugin);
 
         #[cfg(feature = "wasm")]
-        app.add_plugins(tdn_bevy::wasm::WasmClientPlugin);
+        app.add_plugins(WasmClientPlugin);
+
+        app.insert_resource(RoomMarket::default());
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Default)]
+pub struct PendingRoom {
+    pub room: u64,
+    pub players: Vec<String>,
+}
+
+/// Fetch pending rooms list from some node in RoomMarket
+#[derive(Default, Resource)]
+pub struct RoomMarket {
+    pub url: String,
+    pub game: String,
+    pub rooms: Vec<PendingRoom>,
+}
+
+/// Fetch pending rooms
+pub fn fetch_room_market(
+    mut commands: Commands,
+    list: Res<RoomMarket>,
+    http_client: Res<HttpClient>,
+) {
+    if !list.url.is_empty() && !list.game.is_empty() {
+        http_client.jsonrpc(
+            &mut commands,
+            &list.url,
+            "room_market",
+            0, // gid
+            json!(vec![&list.game]),
+            vec![(
+                "peer".to_owned(),
+                json!("0x0000000000000000000000000000000000000000"),
+            )],
+        );
+    }
+}
+
+/// Handle the room market response
+pub fn handle_room_market(
+    mut commands: Commands,
+    mut list: ResMut<RoomMarket>,
+    connections: Query<(Entity, &HttpConnection)>,
+) {
+    for (entity, connection) in &connections {
+        if &connection.method == "room_market" {
+            match connection.recv() {
+                Ok(Ok(value)) => {
+                    if let Ok(rooms) = serde_json::from_value::<Vec<PendingRoom>>(value) {
+                        list.rooms = rooms;
+                    }
+                }
+                Ok(Err(error)) => {
+                    error!("Room Market ERROR: {}", error);
+                    commands.entity(entity).despawn()
+                }
+                Err(RecvError::Empty) => {}
+                Err(RecvError::Closed) => commands.entity(entity).despawn(),
+            }
+        }
     }
 }
 
